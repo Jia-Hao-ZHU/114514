b1fd44bf4a99c6495c8d1d87d6082c5ecb4849ee
Jia-Hao-ZHU <jiahao.zhu26@ycdsbk12.ca>
2024-09-23 10:50:20 -0400

    Add files via upload

---
+++ tracing numerical variables.py
@@ -0,0 +1,29 @@
+# a = 4
+# b = 1
+# c = b / a
+# d = b // a
+# print(a)
+
+# a = 5
+# a = a + 2
+# print(a)
+# b = a
+# a = a + 1
+# b = b + a
+# print(b)
+
+# a = 8
+# a += 3
+# print(a)
+# print(a+3)
+# b = a
+# a += 2
+# b += a
+
+total = 0
+print(total)
+total += 1
+print(total)
+total += 1
+total += 1
+total += 1
